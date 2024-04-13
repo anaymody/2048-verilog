7a0d751444ee915023fca1ca8fda9f6916dfd35d
Shriram Krishnamachari <shrikris@usc.edu>
2024-04-13 14:49:30 -0700

    sm.v

--- state_machine.v
+++ state_machine.v
@@ -1,15 +1,106 @@
 
-//set up board
-reg[11:0] board [3:0][3:0];
-reg[11:0] temp;
+//module
+module ee354_2048(Clk, Reset, q_I, q_Wait, q_Up, q_Down, q_Right, q_Left, q_Win, q_Lose);
+
+
+//inputs
+input Clk, Reset;
+
+//outputs
+output q_I, q_Wait, q_Up, q_Down, q_Right, q_Left, q_Win, q_Lose;
+reg[7:0] state;
+assign {q_I, q_Wait, q_Up, q_Down, q_Right, q_Left, q_Win, q_Lose} = state;
+
+localparam
+I = 8'b00000001, WAIT = 8'b00000010, UP = 8'b00000100, DOWN = 8'b00001000, 
+RIGHT = 8'b00010000, LEFT = 8'b00100000, WIN = 8'b01000000, LOSE = 8'b10000000;
+
+reg[10:0] board [3:0][3:0];
+reg[10:0] temp;
 integer i, j;
 
-initial begin
-    for (i = 0; i < 4; i = i+1) begin
-        for (j = 0; j < 4; j = j+1) begin
-            board[i][j] = 0;
-        end
+always @ (posedge Clk, posedge Reset)
+begin
+    if(Reset)
+    begin
+        state <= I;
     end
+
+    else
+        case(state)
+            I:
+            begin
+            //state transitions
+
+            //data transitions
+                for (i = 0; i < 4; i = i+1) begin
+                    for (j = 0; j < 4; j = j+1) begin
+                        board[i][j] <= 0;
+                    end
+                end
+            end
+
+            WAIT:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+
+            UP:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+
+            DOWN:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+
+            RIGHT:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+
+            LEFT:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+
+            WIN:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+
+            LOSE:
+            begin
+            //state transitions
+
+            //data transitions
+
+            end
+        endcase
+
+end
+
+initial begin
+    
 end
 
 task moveUp();
